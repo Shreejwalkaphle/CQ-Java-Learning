caa7d9a917b59cd83a5359e9958c0a4bd173e175
Shreejwal Kaphle <shreejwal.kaphle@compliancequest.com>
2024-05-28 11:26:50 +0545

    [commit-1] | working client credential flow for salesforce and java application to get the access token

--- src/main/java/com/example/Integration2/AuthenticationAndAuthorization/SalesforceConfig.java
+++ src/main/java/com/example/Integration2/AuthenticationAndAuthorization/SalesforceConfig.java
@@ -1,21 +1,27 @@
-//package com.example.Integration2.AuthenticationAndAuthorization;
-//import lombok.Getter;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.stereotype.Component;
-//
-//@Getter
-//@Configuration
-//public class SalesforceConfig {
-//
-//    @Value("${salesforce.ac.consumer-key}")
-//    private String consumerKey;
-//
-//    @Value("${salesforce.ac.consumer-secret}")
-//    private String consumerSecret;
-//
-//    @Value("${salesforce.ac.redirect-uri}")
-//    private String redirectUri;
-//
-//
-//}
+package com.example.Integration2.AuthenticationAndAuthorization;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+/*
+* this class fetches the configuration details from application.properties file
+*/
+@Getter
+@Configuration
+public class SalesforceConfig {
+
+    @Value("${salesforce.cc.consumer-key}")
+    private String consumerKey;
+
+    @Value("${salesforce.cc.consumer-secret}")
+    private String consumerSecret;
+
+    @Value("${TOKEN_URL}")
+    private String tokenUrl;
+
+    @Value("${ORG_BASE_URL}")
+    private String orgBaseUrl;
+
+    @Value("${GRANT_TYPE}")
+    private String grantType;
+
+}
