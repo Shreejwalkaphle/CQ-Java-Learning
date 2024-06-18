7b0a42f4b361786646c15a550db0c2d0702c7d85
Shreejwal Kaphle <shreejwal.kaphle@compliancequest.com>
2024-06-18 17:22:52 +0545

    [commit-3] | RabbitMqSetup from scratch. for Direct Exchange Type, using RabbitAdmin and Connection Factory. single queue linked to single Direct Exchange using Single routing Key.

--- src/main/java/com/example/Integration2/rabbitMQconfigurations/RabbitMQConfiguration.java
+++ src/main/java/com/example/Integration2/rabbitMQconfigurations/RabbitMQConfiguration.java
@@ -1,10 +1,9 @@
 package com.example.Integration2.rabbitMQconfigurations;
 
 import com.example.Integration2.Utility.PropertiesFileReader;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,18 +14,25 @@ public class RabbitMQConfiguration {
     @Autowired
     PropertiesFileReader fromPropertiesFile;
 
+//    properties file ma rabbit ko connection ko details spring le read garera
+//    rabbit message broker sanga connection create gariori yaha connectionfactory variable ma
+//    tyo rabbit message broker sanga ko connection ko instance lera ayera rakhdincha.
+
+    @Autowired
+    ConnectionFactory connectionFactory;
+
+//    yo connection ko instance le ani hamile RabbitAdmin ko instance create garna sakcham.
     @Bean
-    public Queue yourQueue() {
-        return new Queue(fromPropertiesFile.getQueueName(), false);
+    public RabbitAdmin rabbitAdmin() {
+        return new RabbitAdmin(connectionFactory);
     }
 
-    @Bean
-    public TopicExchange yourExchange() {
-        return new TopicExchange(fromPropertiesFile.getExchangeName());
-    }
-
-    @Bean
-    public Binding binding(Queue yourQueue, TopicExchange yourExchange) {
-        return BindingBuilder.bind(yourQueue).to(yourExchange).with(fromPropertiesFile.getRoutingKey());
+    public void declareQueueExchangesAndBindings(){
+        Queue yourQueue = new Queue(fromPropertiesFile.getQueueName());
+        rabbitAdmin().declareQueue(yourQueue);
+        DirectExchange yourExchange = new DirectExchange(fromPropertiesFile.getExchangeName());
+        rabbitAdmin().declareExchange(yourExchange);
+        Binding binding = BindingBuilder.bind(yourQueue).to(yourExchange).with(fromPropertiesFile.getRoutingKey());
+        rabbitAdmin().declareBinding(binding);
     }
 }
