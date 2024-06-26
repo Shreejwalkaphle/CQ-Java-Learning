6e5e5a7b3cc18b092cb1f1fa716de62808098723
Shreejwal Kaphle <shreejwal.kaphle@compliancequest.com>
2024-06-26 17:49:05 +0545

    [commit-2] | added dependency for swagger open ui. added models to demonstrate the spring api documentation. hit the getUsers api from the swagger 'http://localhost:8080/swagger-ui/index.html' to get the documentation.

--- src/main/java/com/example/Integration2/Controllers/TestController.java
+++ src/main/java/com/example/Integration2/Controllers/TestController.java
@@ -1,8 +1,12 @@
 package com.example.Integration2.Controllers;
 
+import com.example.Integration2.models.Users;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping()
@@ -18,5 +22,22 @@ public class TestController {
 
         // Return a simple greeting message
         return "Hello! Welcome to the Java application's first endpoint.";
+    }
+    @GetMapping("/getUsers")
+    @CrossOrigin("*")
+    public Users getUser() {
+        Users user = new Users();
+        user.setUsername("exampleUser");
+        user.setPassword("examplePassword");
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setEmail("johndoe@example.com");
+        user.setRole("admin");
+        user.setDateOfBirth(LocalDate.of(1990, 1, 1));
+        user.setAddress("Random Address");
+        user.setPhoneNumber("1234567890");
+        user.setTasks(Arrays.asList("Task 1", "Task 2", "Task 3"));
+
+        return user;
     }
 }
---
+++ src/main/java/com/example/Integration2/models/Users.java
@@ -0,0 +1,42 @@
+package com.example.Integration2.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+public class Users {
+    @NotBlank(message = "Username is mandatory")
+    private String username;
+
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+    @NotBlank(message = "First name is mandatory")
+    private String firstName;
+
+    @NotBlank(message = "Last name is mandatory")
+    private String lastName;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    @NotBlank(message = "Role is mandatory")
+    private String role;
+
+    private LocalDate dateOfBirth;
+
+    private String address;
+
+    private String phoneNumber;
+
+    private List<String> tasks;
+}
