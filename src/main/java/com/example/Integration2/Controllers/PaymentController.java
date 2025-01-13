112b3533a16dfa5c0df6a1ef5d69fe3143803850
Shreejwal Kaphle <shreejwal.kaphle@compliancequest.com>
2025-01-13 11:00:05 +0545

    [commit-3] | bank application was made so continued, ecommerce application will hit this api, it will give the payment gateway angular application's url. frontend will redirect to that url

--- src/main/java/com/example/Integration2/Controllers/PaymentController.java
+++ src/main/java/com/example/Integration2/Controllers/PaymentController.java
@@ -11,29 +11,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/payment")
 public class PaymentController {
-
-//	@PostMapping("/order")
-//	@CrossOrigin("http://localhost:4200/")
-//	public ResponseEntity<String> placeOrder(@RequestBody Product product) {
-//		// Logging the received product details
-//		System.out.println("Received order for product: " + product);
-//
-//		// Simulate payment processing (mock for now)
-//		boolean paymentSuccess = processPayment(product);
-//
-//		if (paymentSuccess) {
-//			return ResponseEntity.ok("Payment successful for product: " + product.getName());
-//		} else {
-//			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-//					.body("Payment failed for product: " + product.getName());
-//		}
-//	}
-//
-//	private boolean processPayment(Product product) {
-//		// Placeholder for actual payment logic
-//		System.out.println("Processing payment for product: " + product.getName());
-//		return true; // Assume payment always succeeds for now
-//	}
 	@PostMapping("/order")
 	@CrossOrigin("http://localhost:4200/")
 	public ResponseEntity< Map <String, String> > createOrder( @RequestBody Map<String, Object> productDetails) {
@@ -41,7 +18,7 @@ public class PaymentController {
 		int price = (Integer) productDetails.get("price");
 
 		// Simulating a payment gateway response
-		String paymentLink = "https://payment-gateway.com/pay?amount=" + price;
+		String paymentLink = "http://localhost:4200?name="+productName+"&amount=" + price;
 
 		Map<String, String> response = new HashMap <>();
 		response.put("message", "Order created for " + productName);
