93b34d5b9b580c601acbffa63c0ff3b7a5d57059
Shreejwal Kaphle <shreejwal.kaphle@compliancequest.com>
2025-01-13 13:33:37 +0545

    mend

--- src/main/java/com/example/Integration2/Controllers/TestController.java
+++ src/main/java/com/example/Integration2/Controllers/TestController.java
@@ -2,6 +2,8 @@ package com.example.Integration2.Controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,12 +13,10 @@ public class TestController {
     private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping()
-    @CrossOrigin("*")
-    public String hello() {
-        // Log a debug message
-        logger.info("Request received at /hello/sayHello endpoint.");
+    public ResponseEntity<String> hello() {
+        // Log a debug messlogger.info("Request received at /hello/sayHello endpoint.");
 
         // Return a simple greeting message
-        return "Hello! Welcome to the Java application's first endpoint.";
+        return new ResponseEntity<>("{\"message\": \"Hello! Welcome to the Java application's first endpoint.\"}", HttpStatus.OK);
     }
 }
